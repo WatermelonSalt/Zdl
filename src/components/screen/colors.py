4b718f0ab4acc96eb0712f3748877de4cbad726f
WatermelonSalt <watermelonsalt682@gmail.com>
2021-07-27 19:04:02 +0530

    Added code to footer

--- src/components/screen/colors.py
+++ src/components/screen/colors.py
@@ -7,3 +7,4 @@ class Colors:
     textbox_active_color = f"{term.yellow}"
     textbox_inactive_color = f"{term.cyan}"
     body_color = f"{term.color_rgb(118, 255, 33)}"
+    footer_color = f"{term.magenta}"
